77299fb32527a238662d720ca75c9584a2b16b74
Nolan Kovacik <noltron000@gmail.com>
2020-02-10 10:47:29 -0800

    [add] javascript magic with audio

--- src/script.js
+++ src/script.js
@@ -1,10 +1,28 @@
+let analyzer
+let frequencyArray
 const playButton = document.getElementById('button-play')
 
 const startAudio = () => {
-	const audio = new Audio()
-	const audioContext = new (window.AudioContext || window.webkitAudioContext)()
+	// TODO: understand what this does a little better
 
-	audio.src = './data/bird-whistling-a.wav'
+	// create an "audio context"
+	// https://developer.mozilla.org/en-US/docs/Web/API/AudioContext
+	const audioContext = new AudioContext()
+	analyzer = audioContext.createAnalyser()
+	analyzer.connect(audioContext.destination)
+
+	// load & apply audio files
+	// https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement/Audio
+	const audio = new Audio('./data/bird-whistling-a.wav')
+	const source = audioContext.createMediaElementSource(audio)
+
+	// connect source audio with the analyzer
+	source.connect(analyzer)
+
+	// track the analyzer's frequency rates
+	frequencyArray = new Uint8Array(analyzer.frequencyBinCount)
+
+	// play the audio
 	audio.play()
 }
 
