baa570824f9b1dbc632e220a18c26d791e877db1
Nolan Kovacik <noltron000@gmail.com>
2020-02-10 10:23:50 -0800

    [add] playing audio

--- src/script.js
+++ src/script.js
@@ -1,8 +1,13 @@
+const playButton = document.getElementById('button-play')
+
 const startAudio = () => {
 	const audio = new Audio()
 	const audioContext = new (window.AudioContext || window.webkitAudioContext)()
 
 	audio.src = './data/bird-whistling-a.wav'
-
 	audio.play()
 }
+
+playButton.addEventListener('click', (ev) => {
+	startAudio()
+})
